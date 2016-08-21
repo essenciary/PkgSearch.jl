3e3a5a8f2f26fa078c2d5f87568931b076928cff
Adrian Salceanu <e@essenciary.com>
2016-08-21 17:02:36 +0200

    added help options; compacted output

--- src/PkgSearch.jl
+++ src/PkgSearch.jl
@@ -27,7 +27,7 @@ const search = SearchState(1, "", Dict{AbstractString, Any}(), Dict{AbstractStri
 """
   lookup(keyword1, keyword2, ...)
 
-Searches for Julia packages matching the provided keywords. Search results limited to $items_per_page per lookup. 
+Searches for Julia packages matching the provided keywords. Search results limited to $items_per_page per lookup.
 Returns the results and displays the info in the REPL.
 
 #Examples
@@ -41,7 +41,7 @@ julia> PkgSearch.lookup("Redis")
 function lookup(keywords...)
   search.terms = join(map(x -> strip(string(x)), collect(keywords)), "+")
   search.page = 1
-  _lookup(search.terms)
+  _lookup(search.terms);
 end
 @memoize function _lookup(keywords::AbstractString; autorender::Bool = true, page::Int = 1)
   if isempty(strip(keywords))
@@ -52,17 +52,17 @@ end
     search.page = 1
   end
 
-  try 
-    page_uri = search_api_uri * search_endpoint * "?q=" * replace(keywords, " ", "+") * "&page[number]=$(search.page)&page[size]=$items_per_page" 
+  try
+    page_uri = search_api_uri * search_endpoint * "?q=" * replace(keywords, " ", "+") * "&page[number]=$(search.page)&page[size]=$items_per_page"
     DEBUG && Lumberjack.info(page_uri)
-    search.results = Requests.get(page_uri) |> Requests.json  
-  catch ex 
-    rethrow(ex) # do something better with the exception 
+    search.results = Requests.get(page_uri) |> Requests.json
+  catch ex
+    rethrow(ex) # do something better with the exception
   end
 
   if ! isempty(search.results["data"])
     return process_results(search.results, autorender = autorender)
-  else 
+  else
     return []
   end
 end
@@ -70,7 +70,7 @@ end
 """
   next([page_jump])
 
-Performs a new lookup to bring the next batch of $items_per_page results, if available. If page_jump is provided, it will go directly to that page. 
+Performs a new lookup to bring the next batch of $items_per_page results, if available. If page_jump is provided, it will go directly to that page.
 """
 function next(page_jump::Int = 1)
   search.page += page_jump
@@ -80,7 +80,7 @@ end
 """
   prev([page_jump])
 
-Performs a new lookup to bring the previous batch of $items_per_page results, if available. If page_jump is provided, it will go directly to that page. 
+Performs a new lookup to bring the previous batch of $items_per_page results, if available. If page_jump is provided, it will go directly to that page.
 """
 function prev(page_jump::Int = 1)
   next(page_jump * -1)
@@ -97,7 +97,7 @@ end
     _lookup(package_name, autorender = false)
   end
 
-  package_details = Requests.get(search_api_uri * details_endpoint * "/" * string(search.packages[package_name]["id"])) |> Requests.json 
+  package_details = Requests.get(search_api_uri * details_endpoint * "/" * string(search.packages[package_name]["id"])) |> Requests.json
 
   if isempty(package_details) error("Package not found") end
   autorender && render_package(package_details["data"]["attributes"])
@@ -106,19 +106,19 @@ end
 end
 
 """
-Installs the requested package using the appropriate technique: Pkg.add if it's an official METADATA package, or Pkg.clone if not. 
+Installs the requested package using the appropriate technique: Pkg.add if it's an official METADATA package, or Pkg.clone if not.
 """
 function install(package_name::AbstractString)
   if is_official_package(package_name)
     Pkg.add(package_name)
-  else 
+  else
     pkg_details = details(package_name, autorender = false)
     Pkg.clone(pkg_details["url"])
   end
 end
 
 """
-Returns true if the requested package is official, that is part of METADATA. False otherwise. 
+Returns true if the requested package is official, that is part of METADATA. False otherwise.
 """
 function is_official_package(package_name::AbstractString)
   haskey(metadata_packages(), package_name)
@@ -150,7 +150,7 @@ end
 
 function process_results(search_results::Dict; autorender::Bool = false)
   for p in search_results["data"]
-    p["search"]["headline"] = replace(p["search"]["headline"], r"(<b>|</b>)", "**") 
+    p["search"]["headline"] = replace(p["search"]["headline"], r"(<b>|</b>)", "**")
     search.packages[p["attributes"]["name"]] = p
     autorender && render(p)
   end
@@ -160,11 +160,7 @@ end
 
 function render{T<:AbstractString, U<:Any}(p::Dict{T,U})
   println("=====================================================")
-  println(p["attributes"]["name"])
-  println("-----------------------------------------------------")
-  println(is_official_package(p["attributes"]["name"]) ? "Official package" : "Unofficial package")
-  println("-----------------------------------------------------")
-  println(p["attributes"]["url"])
+  println(mark_official_package(p["attributes"]["name"]) * " " * p["attributes"]["name"] * " " * p["attributes"]["url"])
   println("-----------------------------------------------------")
   display(p["search"]["headline"] |> Markdown.parse)
   println("_____________________________________________________\n\n")
@@ -172,11 +168,7 @@ end
 
 function render_package{T<:AbstractString, U<:Any}(p::Dict{T,U})
   println("=====================================================")
-  println(p["name"])
-  println("-----------------------------------------------------")
-  println(p["url"])
-  println("-----------------------------------------------------")
-  println(is_official_package(p["name"]) ? "Official package" : "Unofficial package")
+  println(mark_official_package(p["name"]) * " " * p["name"] * " " * p["url"])
   println("-----------------------------------------------------")
   println("Weekly activity: ")
   spark(p["participation"])
@@ -184,9 +176,31 @@ function render_package{T<:AbstractString, U<:Any}(p::Dict{T,U})
   println("-----------------------------------------------------")
   display(replace(p["readme"], r"(<b>|</b>)", "**") |> Markdown.parse)
   println("_____________________________________________________\n\n")
-end 
+end
+
+function mark_official_package(package_name::AbstractString)
+  is_official_package(package_name) ? "✅ " : "⚠️ "
+end
+
+function help()
+  println("""\n
+    PkgSearch quick options: \n
+    * PkgSearch.lookup("words...")
+      Returns the first page of results matching the search terms \n
+    * PkgSearch.next()
+      Returns the next page of search results \n
+    * PkgSearch.prev()
+      Returns the previous page of search results \n
+    * PkgSearch.details("package name")
+      Shows the details for "package name" \n
+    * Legend:
+      ✅   official package (metadata)
+      ⚠️   unofficial package
+  """)
+end
 
 println("Preloading METADATA packages... please wait...")
 metadata_packages()
+println("Ready - use `PkgSearch.help()` for help.")
 
 end
